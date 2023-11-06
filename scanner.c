f407ba834d85a64445baa5b18cdbcf44ac6aa299
Samuel Huang <samuel.g.huang@outlook.com>
2023-11-06 11:58:16 -0500

    Modify scan() to take file pointer

--- scanner.c
+++ scanner.c
@@ -5,25 +5,16 @@ extern FILE* yyin;
 extern int yylex();
 extern char* yytext;
 
-int scan(const char* filename)
+int scan(FILE* fp)
 {
-	yyin = fopen(filename, "r");
-	if (yyin == NULL)
-	{
-		printf("Could not open file %s\n", filename);
-		return 1;
-	}
-
+	yyin = fp;
 	int token = 1; // Initialize to a non-zero value
 	while (token != TOKEN_EOF)
 	{
 		// Handle the tokens
-		if (token == TOKEN_ERROR) {
+		if (token == TOKEN_ERROR)
 			return 1;
-		}
 		token = yylex();
 	}
-
-	fclose(yyin);
 	return 0;
 }
