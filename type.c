3d457088110398cf236b47c0b0977017ac0844b4
Samuel Huang <samuel.g.huang@outlook.com>
2023-11-06 12:00:54 -0500

    Add type_create() and type_print()

---
+++ type.c
@@ -0,0 +1,83 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "type.h"
+
+/* Creating the types */
+
+struct type* type_create(type_t kind)
+{
+	struct type* t = malloc(sizeof(*t));
+	t->kind = kind;
+	return t;
+}
+
+struct type* type_create_array(struct type* subtype, struct expr* size)
+{
+	struct type* t = type_create(TYPE_ARRAY);
+	t->subtype = subtype;
+	t->size = size;
+	return t;
+}
+
+struct type* type_create_func(struct type* rtype, struct param_list* params)
+{
+	struct type* t = type_create(TYPE_FUNCTION);
+	t->subtype = rtype;
+	t->params = params;
+	return t;
+}
+
+/* Printing the types */
+
+void type_print_primitive(struct type* t)
+{
+	switch (t->kind)
+	{
+	case TYPE_INTEGER:
+		printf("integer");
+		break;
+	case TYPE_FLOAT:
+		printf("float");
+		break;
+	case TYPE_CHAR:
+		printf("char");
+		break;
+	case TYPE_VOID:
+		printf("void");
+		break;
+	case TYPE_BOOLEAN:
+		printf("boolean");
+		break;
+	case TYPE_STRING:
+		printf("string");
+		break;
+	default:
+		/* Compound types handled in type_print() */
+		break;
+	}
+}
+
+void type_print(struct type* t)
+{
+	switch (t->kind)
+	{
+	case TYPE_ARRAY:
+		printf("array [");
+		if (t->size != NULL)
+			expr_print(t->size);
+		printf("] ");
+		type_print(t->subtype);
+		break;
+	case TYPE_FUNCTION:
+		printf("function ");
+		type_print(t->subtype);
+		printf(" (");
+		param_list_print(t->params);
+		printf(")");
+		break;
+	default:
+		/* Primitive types */
+		type_print_primitive(t);
+		break;
+	}
+}
--- type.h
+++ type.h
@@ -2,24 +2,33 @@
 #define TYPE_H
 
 #include "param_list.h"
+#include "expr.h"
 
-typedef enum {
+typedef enum
+{
 	TYPE_VOID,
 	TYPE_BOOLEAN,
-	TYPE_CHARACTER,
+	TYPE_CHAR,
 	TYPE_INTEGER,
+	TYPE_FLOAT,
 	TYPE_STRING,
 	TYPE_ARRAY,
 	TYPE_FUNCTION,
 } type_t;
 
-struct type {
+struct type
+{
 	type_t kind;
-	struct param_list *params;
-	struct type *subtype;
+	struct type* subtype;
+	struct expr* size;
+	struct param_list* params;
 };
 
-struct type * type_create( type_t kind, struct type *subtype, struct param_list *params );
-void          type_print( struct type *t );
+/* Creating types */
+struct type* type_create(type_t kind);
+struct type* type_create_array(struct type* subtype, struct expr* size);
+struct type* type_create_func(struct type* rtype, struct param_list* params);
+
+void type_print(struct type* t);
 
 #endif
