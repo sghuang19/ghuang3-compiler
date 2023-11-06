5544f44b8141d8f1d7dce096ad9d65cf23605111
Samuel Huang <samuel.g.huang@outlook.com>
2023-11-06 11:57:27 -0500

    Modify parse() to support parsing consecutive files

--- parser.c
+++ parser.c
@@ -2,13 +2,14 @@
 #include "parser.h"
 
 extern int yyparse();
-extern FILE* yyin;
+extern void yyrestart(FILE*);
 extern int yydebug;
 extern struct decl* root;
 
 struct decl* parse(FILE* fp)
 {
 	yydebug = 0;
+	yyrestart(fp);
 	yyparse();
 	return root;
 }
