ca9a1793b35367e2f089a546f2bd2739222b29aa
Samuel Huang <samuel.g.huang@outlook.com>
2023-11-06 11:56:02 -0500

    Modify parse() to take file pointer

--- parser.c
+++ parser.c
@@ -4,16 +4,9 @@ extern int yyparse();
 extern FILE* yyin;
 extern int yydebug;
 
-int parse(const char* filename)
+struct decl* parse(FILE* fp)
 {
 	yydebug = 0;
-	yyin = fopen(filename, "r");
-	if (!yyin)
-	{
-		fprintf(stderr, "Could not open file %s\n", filename);
-		return 1;
-	}
 	int result = yyparse();
-	fclose(yyin);
 	return result;
 }
