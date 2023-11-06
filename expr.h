05d87b41b82e36aac6efde971caeee3163531583
Samuel Huang <samuel.g.huang@outlook.com>
2023-11-06 12:02:13 -0500

    Add expr_create() and expr_print()

--- expr.h
+++ expr.h
@@ -3,35 +3,77 @@
 
 #include "symbol.h"
 
-typedef enum {
+typedef enum
+{
+	/* Arithmetic operator nodes */
 	EXPR_ADD,
 	EXPR_SUB,
 	EXPR_MUL,
-	EXPR_DIV
-	/* many more kinds of exprs to add here */
+	EXPR_DIV,
+	EXPR_MOD,
+	EXPR_EXP,
+
+	/* Comparison and logic operator nodes */
+	EXPR_GT,
+	EXPR_GEQ,
+	EXPR_LT,
+	EXPR_LEQ,
+	EXPR_EQ,
+	EXPR_NEQ,
+	EXPR_AND,
+	EXPR_OR,
+
+	/* Other binary operator nodes */
+	EXPR_CALL,
+	EXPR_INDEX,
+	EXPR_ASSIGN,
+
+	/* Unary operator nodes */
+	EXPR_NEG,
+	EXPR_NOT,
+	EXPR_INCREMENT,
+	EXPR_DECREMENT,
+
+	/* Leaf nodes */
+	EXPR_INTEGER_LITERAL,
+	EXPR_FLOAT_LITERAL,
+	EXPR_BOOLEAN_LITERAL,
+	EXPR_CHAR_LITERAL,
+	EXPR_STRING_LITERAL,
+	EXPR_NAME,
+
+	/* Special nodes */
+	EXPR_LIST
 } expr_t;
 
-struct expr {
-	/* used by all kinds of exprs */
+struct expr
+{
+	/* Used by all kinds of expressions */
 	expr_t kind;
-	struct expr *left;
-	struct expr *right;
+	struct expr* left;
+	struct expr* right;
 
-	/* used by various leaf exprs */
-	const char *name;
-	int literal_value;
-	const char * string_literal;
-	struct symbol *symbol;
+	/* Used by various leaf expressions */
+	struct symbol* symbol;
+	const char* name;
+	int integer_literal; // Used by INTEGER and BOOLEAN
+	float float_literal;
+	char char_literal;
+	const char* string_literal;
 };
 
-struct expr * expr_create( expr_t kind, struct expr *left, struct expr *right );
+struct expr* expr_create(expr_t kind, struct expr* left, struct expr* right);
+struct expr* expr_create_unary(expr_t kind, struct expr* operand);
 
-struct expr * expr_create_name( const char *n );
-struct expr * expr_create_integer_literal( int c );
-struct expr * expr_create_boolean_literal( int c );
-struct expr * expr_create_char_literal( char c );
-struct expr * expr_create_string_literal( const char *str );
+/* Creating leaf nodes */
+struct expr* expr_create_name(const char* n);
+struct expr* expr_create_integer_literal(int i);
+struct expr* expr_create_float_literal(float f);
+struct expr* expr_create_boolean_literal(int b);
+struct expr* expr_create_char_literal(char c);
+struct expr* expr_create_string_literal(const char* str);
 
-void expr_print( struct expr *e );
+/* Printing the expressions */
+void expr_print(const struct expr* e);
 
 #endif
