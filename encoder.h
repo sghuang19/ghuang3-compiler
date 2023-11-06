4680045fd5cf152533327391af420b17638143e9
Samuel Huang <samuel.g.huang@outlook.com>
2023-11-06 11:52:46 -0500

    Modify decode() to take file pointer

--- encoder.h
+++ encoder.h
@@ -1,7 +1,12 @@
+#ifndef ENCODER_H
+#define ENCODER_H
+
 #define MAX_STRING_LEN 255
 
 int string_decode(const char* es, char* s);
 
 int string_encode(const char* s, char* es);
 
-int decode(const char* filename);
+int decode(FILE *fp);
+
+#endif
