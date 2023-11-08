1bf6596e3abdc870bf3a2c888238bd96e541d007
Samuel Huang <samuel.g.huang@outlook.com>
2023-11-08 12:29:49 -0500

    Implement param_list_resolve()

--- param_list.c
+++ param_list.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "param_list.h"
+#include "scope.h"
 
 struct param_list* param_list_create(char* name, struct type* type, struct param_list* next)
 {
@@ -21,4 +22,23 @@ void param_list_print(struct param_list* p)
 		printf(", ");
 		param_list_print(p->next);
 	}
+}
+
+void param_list_resolve(struct param_list* p)
+{
+	if (!p)
+	{
+		cur_param = 0;
+		return;
+	}
+	if (scope_lookup_current(p->name))
+	{
+		fprintf(stderr, "Resolve Error | duplicate parameter '%s'\n", p->name);
+		res_errors++;
+	}
+	struct symbol* s = symbol_create(SYMBOL_PARAM, p->type, p->name, cur_param++);
+	scope_bind(p->name, s);
+	if (p->type->kind == TYPE_ARRAY)
+		expr_resolve(p->type->size);
+	param_list_resolve(p->next);
 }
